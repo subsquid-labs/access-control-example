045af59b24ec74976363f76422b6ad5b8e778a02
abernatskiy <abernatskiy@toha.li>
2023-04-04 23:46:24 +0900

    Cleanup
    	modified:   src/server-extension/resolvers.ts

--- src/server-extension/resolvers.ts
+++ src/server-extension/resolvers.ts
@@ -11,34 +11,34 @@ import { UserComment } from '../model/generated'
 
 @ObjectType()
 export class UserCommentCountQueryResult {
-  @Field(() => Number, { nullable: false })
-  total!: number
+	@Field(() => Number, { nullable: false })
+	total!: number
 
-  constructor(props: Partial<UserCommentCountQueryResult>) {
-    Object.assign(this, props);
-  }
+	constructor(props: Partial<UserCommentCountQueryResult>) {
+		Object.assign(this, props);
+	}
 }
 
 @Resolver()
 export class UserCommentResolver {
-  // Set by depenency injection
-  constructor(private tx: () => Promise<EntityManager>) {}
+	constructor(private tx: () => Promise<EntityManager>) {}
 
-  @Query(() => [UserCommentCountQueryResult])
-  async countUserComments(): Promise<UserCommentCountQueryResult[]> {
-    const manager = await this.tx()
-    // execute custom SQL query
-    const result: UserCommentCountQueryResult[] = await manager.getRepository(UserComment).query(
-      `SELECT 
-        COUNT(id) as total 
-      FROM user_comment`)
-    return result
-  }
+	@Query(() => [UserCommentCountQueryResult])
+	async countUserComments(): Promise<UserCommentCountQueryResult[]> {
+		const manager = await this.tx()
+		const result: UserCommentCountQueryResult[] = await manager.getRepository(UserComment).query(`SELECT COUNT(id) as total FROM user_comment`)
+		return result
+	}
 
 	@Mutation(() => Boolean)
-	async addComment(@Arg('text') commentText: string): Promise<Boolean> {
+	async addComment(@Arg('text') comment: string): Promise<Boolean> {
 		let manager = await this.tx()
-		await manager.save(new UserComment({id: 'fsdfsdfsdfsd', user: 'anonymous', comment: commentText}))
+		let user = 'anonymous'
+		await manager.save(new UserComment({
+			id: `${user}-${comment}`,
+			user,
+			comment
+		}))
 		return true
 	}
 }
