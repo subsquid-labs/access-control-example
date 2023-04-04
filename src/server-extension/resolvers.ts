2aa8c111279d9b4d2026cdf840ec2e358f13c88e
abernatskiy <abernatskiy@toha.li>
2023-04-04 22:57:06 +0900

    Converting the custom resolver to serve UserComments data
    	modified:   src/server-extension/resolvers.ts

--- src/server-extension/resolvers.ts
+++ src/server-extension/resolvers.ts
@@ -1,30 +1,37 @@
-import { Arg, Field, ObjectType, Query, Resolver } from 'type-graphql'
+import {
+	Arg,
+	Field,
+	ObjectType,
+	InputType,
+	Query,
+	Resolver
+} from 'type-graphql'
 import type { EntityManager } from 'typeorm'
-import { LiquidationEvent } from '../model/generated'
+import { UserComment } from '../model/generated'
 
 @ObjectType()
-export class LiquidationEventQueryResult {
+export class UserCommentCountQueryResult {
   @Field(() => Number, { nullable: false })
   total!: number
 
-  constructor(props: Partial<LiquidationEventQueryResult>) {
+  constructor(props: Partial<UserCommentCountQueryResult>) {
     Object.assign(this, props);
   }
 }
 
 @Resolver()
-export class MyResolver {
+export class UserCommentResolver {
   // Set by depenency injection
   constructor(private tx: () => Promise<EntityManager>) {}
 
-  @Query(() => [LiquidationEventQueryResult])
-  async myQuery(): Promise<LiquidationEventQueryResult[]> {
+  @Query(() => [UserCommentCountQueryResult])
+  async countUserComments(): Promise<UserCommentCountQueryResult[]> {
     const manager = await this.tx()
     // execute custom SQL query
-    const result: LiquidationEventQueryResult[] = await manager.getRepository(LiquidationEvent).query(
+    const result: UserCommentCountQueryResult[] = await manager.getRepository(UserComment).query(
       `SELECT 
         COUNT(id) as total 
-      FROM liquidation_event`)
+      FROM user_comment`)
     return result
   }
 }
